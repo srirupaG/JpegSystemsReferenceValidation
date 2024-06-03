ce4ec3f8749c2e35045ffb954a7732b9ba1aa735
Srirupa Ghoshal <srirupaghoshal@Srirupas-Laptop.local>
2024-06-03 22:57:54 +0200

    Initial commit

---
+++ src/main/java/com/example/jepgSystemsValidation/JpegSystemsReferenceValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jepgSystemsValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpegSystemsReferenceValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpegSystemsReferenceValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jepgSystemsValidation/JpegSystemsReferenceValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jepgSystemsValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpegSystemsReferenceValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
